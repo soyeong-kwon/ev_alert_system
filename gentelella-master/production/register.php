64b0dd99b799a849b6450709c786dc2943df3106
jaemin-Yoo <iamjm29@naver.com>
2020-08-07 00:18:00 +0900

    register.php - Database save

---
+++ gentelella-master/production/register.php
@@ -0,0 +1,14 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+$conn = new mysqli("dajung-db.c6ivw6dubpql.ap-northeast-2.rds.amazonaws.com","dajung","12345678","DajungDB");
+mysqli_query($conn,'SET NAMES utf8');
+
+$name = $_POST['name'];
+$phonenum = $_POST['phonenum'];
+$cartype = $_POST['cartype'];
+$IMEI = $_POST['IMEI'];
+$sql= "insert into personinfo(name, phonenum, cartype, IMEI) values('$name','$phonenum','$cartype','$IMEI')";
+$res= $conn->query($sql);
+
+echo "<script> location.href='form_validation.html'; </script>"
+?>
