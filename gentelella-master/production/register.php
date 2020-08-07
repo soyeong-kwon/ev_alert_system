f628d1d29339129e598b2e549fdb146267c99c66
jaemin-Yoo <iamjm29@naver.com>
2020-08-07 15:19:31 +0900

    table 목록 출력

--- gentelella-master/production/register.php
+++ gentelella-master/production/register.php
@@ -1,5 +1,4 @@
 <?php
-header("Content-Type: text/html; charset=UTF-8");
 $conn = new mysqli("dajung-db.c6ivw6dubpql.ap-northeast-2.rds.amazonaws.com","dajung","12345678","DajungDB");
 mysqli_query($conn,'SET NAMES utf8');
 
