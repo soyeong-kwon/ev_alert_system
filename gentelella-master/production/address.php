64ee98725b4e0f8456af51ae2a37f6b70c2b02b7
jaemin-Yoo <iamjm29@naver.com>
2020-08-11 17:18:06 +0900

    address.php error modify

--- gentelella-master/production/address.php
+++ gentelella-master/production/address.php
@@ -12,19 +12,21 @@ $sql2= "insert into address(Latitude, Longitude) values('$latitude','$longitude'
 $sql3= "select * from address";
 
 $res1= $conn->query($sql1);
-$res2= $conn->query($sql3);
+$res3= $conn->query($sql3);
 
 $response = array();
 $response["success"]=false;
 
+// DB에 IMEI가 있을 때, 위치정보 DB에 저장
 if(mysqli_fetch_array($res1)!=NULL){
     $res2= $conn->query($sql2);
     $response["success"]=true;
 }
 
-while($row=mysqli_fetch_array($res2)){
-    $response["Latitude"]= $row['Latitude'];
-    $response["Longitude"]= $row['Longitude'];
+// 위치정보 앱에 뿌려주기
+while($row=mysqli_fetch_array($res3)){
+    $response["Latitude"]= $row["Latitude"];
+    $response["Longitude"]= $row["Longitude"];
 }
 
 echo json_encode($response);
