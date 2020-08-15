4efadf7c66a670b4bae5f361bcaf00f923bd3c98
jaemin-Yoo <iamjm29@naver.com>
2020-08-16 00:48:39 +0900

    Remove data -> Update date [address.php]

--- gentelella-master/production/address.php
+++ gentelella-master/production/address.php
@@ -5,23 +5,33 @@ mysqli_query($conn,'SET NAMES utf8');
 
 $latitude = $_POST['Latitude'];
 $longitude = $_POST['Longitude'];
-$phonenum = $_POST['phonenum'];
+$phonenum = $_POST['PhoneNum'];
 
+$sql= "select * from address where phonenum = '$phonenum'";
 $sql1= "select * from personinfo where phonenum = '$phonenum'";
-$sql2= "insert into address(Latitude, Longitude) values('$latitude','$longitude')";
+$sql2= "insert into address(phonenum, Latitude, Longitude) values('$phonenum', '$latitude','$longitude')";
 $sql3= "select * from address";
-$sql4= "delete from address";
+$sql_update1 = "update address set Latitude='$latitude' where phonenum='$phonenum'";
+$sql_update2 = "update address set Longitude='$longitude' where phonenum='$phonenum'";
 
 
 $response = array();
-$response["success"]=false;
+$response["emergency"]=false;
 
 
+$res= $conn->query($sql);
 $res1= $conn->query($sql1);
-// DB에 IMEI가 있을 때, 위치정보 DB에 저장
+// DB에 phonenum가 있고, 이미 insert 된 phonenum이 없을 때, 위치정보 DB에 저장
 if(mysqli_fetch_array($res1)!=NULL){
-    $res2= $conn->query($sql2);
-    $response["success"]=true;
+    if(mysqli_fetch_array($res)==NULL){
+        $res2= $conn->query($sql2);
+    }
+
+    else{
+        $res_lat= $conn->query($sql_update1);
+        $res_lng= $conn->query($sql_update2);
+    }
+    $response["emergency"]=true;
 }
 
 
