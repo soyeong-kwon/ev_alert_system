e4448f2ab376168deaf278047fdd4002fed421d1
kthink03 <gwonso4502@gmail.com>
2020-09-01 15:52:12 +0900

    temp_delete.php

--- gentelella-master/production/delete.php
+++ gentelella-master/production/delete.php
@@ -3,19 +3,23 @@ header("Content-Type: text/html; charset=UTF-8");
 $conn=new mysqli("dajung-db.c6ivw6dubpql.ap-northeast-2.rds.amazonaws.com","dajung","12345678","DajungDB");
 mysqli_query($conn,'SET NAMES utf8');
 
+$name=$_POST['name'];
 $phonenum=$_POST['phonenum'];
 
-$sql="delete from personinfo where phonenum='$phonenum'";
-$sql1="delete from address where phonenum='$phonenum'";
-$sql2="alter table personinfo auto_increment=1";
-$sql3="set @count=0";
-$sql4="update personinfo set id=@count:=@count+1";
+$personinfo_name="select from personinfo where name='$name'";
+$personinfo_phone="select from personinfo where phonenum='$phonenum'";
+$delete_personinfo="delete from personinfo where name='$name";
+$delete_address="delete from address where phonenum='$phonenum";
 
-$res=$conn->query($sql);
-$res=$conn->query($sql1);
-$res=$conn->query($sql2);
-$res=$conn->query($sql3);
-$res=$conn->query($sql4);
+$res_1=$conn->query($personinfo_name);
+$res_2=$conn->query($personinfo_phone);
+
+if($res_1==$res_2)
+{
+    $res_del1=$conn->query($delete_address);
+    $res_del1=$conn->query($delete_personinfo;
+        
+}
 
 echo "<script>location.href='tables.html'; </script>"
 ?>
