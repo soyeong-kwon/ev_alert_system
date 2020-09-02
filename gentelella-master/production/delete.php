6307f32ed83f4cb2490c8a8d9d5bc54f07ab0951
jaemin-Yoo <iamjm29@naver.com>
2020-09-02 21:25:11 +0900

    modal notice

--- gentelella-master/production/delete.php
+++ gentelella-master/production/delete.php
@@ -3,19 +3,31 @@ header("Content-Type: text/html; charset=UTF-8");
 $conn=new mysqli("dajung-db.c6ivw6dubpql.ap-northeast-2.rds.amazonaws.com","dajung","12345678","DajungDB");
 mysqli_query($conn,'SET NAMES utf8');
 
+$name=$_POST['name'];
 $phonenum=$_POST['phonenum'];
 
-$sql="delete from personinfo where phonenum='$phonenum'";
-$sql1="delete from address where phonenum='$phonenum'";
+$personinfo_check="select * from personinfo where name='$name' and phonenum='$phonenum'";
+$delete_check="delete from personinfo where name='$name' and phonenum='$phonenum'";
 $sql2="alter table personinfo auto_increment=1";
 $sql3="set @count=0";
 $sql4="update personinfo set id=@count:=@count+1";
 
-$res=$conn->query($sql);
-$res=$conn->query($sql1);
-$res=$conn->query($sql2);
-$res=$conn->query($sql3);
-$res=$conn->query($sql4);
+$res_1=$conn->query($personinfo_check);
 
-echo "<script>location.href='tables.html'; </script>"
+if(mysqli_fetch_array($res_1)!=NULL)
+{
+    $res_del1=$conn->query($delete_check);  
+    $res=$conn->query($sql2);
+    $res=$conn->query($sql3);
+    $res=$conn->query($sql4);
+   
+    echo "<script>location.href = 'delete.html';</script>";
+}
+else{
+    echo "<script>alert('등록된 정보가 없습니다.');</script>";
+    echo "<script>location.href = 'delete.html';</script>";
+}
+
+
+
 ?>
