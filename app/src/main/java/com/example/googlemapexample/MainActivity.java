36f1ccbb8830b1d6bc8e34ec01c08c7e4c7ab9f9
ksy1203k <ksy1203k@naver.com>
2020-10-25 16:01:40 +0900

    end

--- app/src/main/java/com/example/googlemapexample/MainActivity.java
+++ app/src/main/java/com/example/googlemapexample/MainActivity.java
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private int emg_button=1;
 
     private  String PhoneNum = "";
-    //private String PhoneNum = "010-9271-3205";
+    //private String PhoneNum = "010-6359-0850";
 
     String[] REQUIRED_PERMISSIONS = {
             Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
@@ -164,10 +164,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             });
         }
-        else{ // 일반 자동차 
+        else{
             emg_button=0;
             Toast.makeText(getApplicationContext(), "일반 자동차",Toast.LENGTH_SHORT).show();
-            tb.setText("버튼을 클릭하실 수 없습니다.");
+            tb.setText("버튼을 사용하실 수 없습니다.");
             tb.setClickable(false);
             Log.d(TAG,"버튼: 사용 불가");
         }
@@ -182,7 +182,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         setDefaultLocation();
         getPermission();
-        
+
         /*
         int hasFineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
         int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
@@ -240,19 +240,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 String markerSnippet = "위도:" + String.valueOf(location.getLatitude()) + " 경도:" + String.valueOf(location.getLongitude());
 
 
-<<<<<<< HEAD
+
                 /* 현재 위치정보 DB저장 */
                 String Latitude = String.valueOf(location.getLatitude()); // 위치정보 받아서 string 변수에 넣기
                 String Longitude = String.valueOf(location.getLongitude());
-
-                String PhoneNum = PhoneNumber;
-=======
-
-                    /* 현재 위치정보 DB저장 */
-                    String Latitude = String.valueOf(location.getLatitude()); // 위치정보 받아서 string 변수에 넣기
-                    String Longitude = String.valueOf(location.getLongitude());
-                    Log.d(TAG, "PhoneNum : "+PhoneNum);
->>>>>>> 38797da9c6e4803795c80173b162e025fc5ec7eb
+                Log.d(TAG, "PhoneNum : "+PhoneNum);
 
                 Log.d(TAG, "PhoneNum : "+PhoneNum);
 
@@ -354,24 +346,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     };
 
-<<<<<<< HEAD
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public void setCurrentLocation(String[] lat, String[] lng) {  //php에 응답. 긴급자동차 위치 마킹하는 함수
-=======
 
 
     public void setCurrentLocation(String[] lat, String[] lng) {
->>>>>>> 38797da9c6e4803795c80173b162e025fc5ec7eb
 
         int i=0;
 
-        while(currentMarker[i]!=null){  //원래 담고있던 마커 정보룰 초기화하는 while
+        while(currentMarker[i]!=null){
             currentMarker[i].remove();
             i++;
         }
 
-        i=0;  //새로 긴급자동차 정보를 담을거야
-        while(lat[i]!=null)  //긴급자동차의 위치가 들어오는 경우에 들어가는 while문
+        i=0;
+        while(lat[i]!=null)
         {
             double latitude = Double.parseDouble(lat[i]);
             double longitude = Double.parseDouble(lng[i]);
@@ -393,18 +380,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             Bitmap smallMarker = Bitmap.createScaledBitmap(b, 70, 50, false); // maker 크기
             markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
 
-<<<<<<< HEAD
-                currentMarker[i] = mMap.addMarker(markerOptions);  //
-           }
-            i++;  //i==1이 되거든
-
-            if(i!=0){
-                currentMarker[i].remove();
-            }  // 마커 사라짐
-=======
             currentMarker[i] = mMap.addMarker(markerOptions);
             i++;
->>>>>>> 38797da9c6e4803795c80173b162e025fc5ec7eb
 
         }
     }
@@ -664,10 +641,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onPause(){
         super.onPause();
-<<<<<<< HEAD
-        String PhoneNum = "010-1111-1111"; //PhoneNumber;
-=======
->>>>>>> 38797da9c6e4803795c80173b162e025fc5ec7eb
 
         Response.Listener<String> responseListener = new Response.Listener<String>() { // php 접속 응답 확인
             @Override
