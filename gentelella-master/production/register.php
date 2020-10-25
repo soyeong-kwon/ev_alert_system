7de52ec9e460ce1fbe478f3a99f8c907cd0a4e94
jaemin-Yoo <iamjm29@naver.com>
2020-10-25 17:10:10 +0900

    approval success

--- gentelella-master/production/register.php
+++ gentelella-master/production/register.php
@@ -8,17 +8,11 @@ mysqli_query($conn,'SET NAMES utf8');
     $emailID = $_POST['emailID'];
     $cartype = $_POST['cartype'];
 
-<<<<<<< HEAD
-    $sql= "insert into personinfo(name, phonenum, emailID, cartype) values('$name','$phonenum', '$emailID', '$cartype')";
-    // personinfo : DB, db에 값을 넣어주는 명령어 sql언어
-    $res= $conn->query($sql); //query문으로 명령어 실행
+    $sql= "insert into approval(name, phonenum, emailID, cartype) values('$name','$phonenum', '$emailID', '$cartype')";
     
-    echo "<script> location.href='register.html'; </script>" // 자바스크립트 언어 : html로 넘어갑니다
-=======
-    $sql= "insert into personinfo(name, phonenum, cartype) values('$name','$phonenum','$cartype')";
-    $sql2="alter table personinfo auto_increment=1";
+    $sql2="alter table approval auto_increment=1";
     $sql3="set @count=0";
-    $sql4="update personinfo set id=@count:=@count+1";
+    $sql4="update approval set id=@count:=@count+1";
     
     $res=$conn->query($sql);
     $res=$conn->query($sql2);
@@ -26,6 +20,5 @@ mysqli_query($conn,'SET NAMES utf8');
     $res=$conn->query($sql4);
 
     echo "<script> location.href='register.html'; </script>"
->>>>>>> 2b7fa438985b4a8b61ca565c3ff99fac427e8b03
 
 ?>
