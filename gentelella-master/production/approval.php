489a96c905d29f6e04aa67148a96c9b31a7029ab
qkrwlals <qkrwlals1213@naver.com>
2020-10-25 16:09:06 +0900

    add approval

---
+++ gentelella-master/production/approval.php
@@ -0,0 +1,22 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+$conn=new mysqli("dajung-db.c6ivw6dubpql.ap-northeast-2.rds.amazonaws.com","dajung","12345678","DajungDB");
+mysqli_query($conn,'SET NAMES utf8');
+
+$phonenum=$_POST['phonenum'];
+
+$sql="delete from approval where phonenum='$phonenum'";
+$sql1="delete from address where phonenum='$phonenum'";
+$sql2="alter table approval auto_increment=1";
+$sql3="set @count=0";
+$sql4="update approval set id=@count:=@count+1";
+
+$res=$conn->query($sql);
+$res=$conn->query($sql1);
+$res=$conn->query($sql2);
+$res=$conn->query($sql3);
+$res=$conn->query($sql4);
+
+echo "<script>location.href='tables.html'; </script>"
+?>
+
