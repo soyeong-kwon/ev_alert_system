5f5e8ea706b0e14a6827a9161a8a8832a31b7770
kthink03 <gwonso4502@gmail.com>
2022-04-15 22:41:01 +0900

    0415 Modify Code

--- gentelella-master/production/turntonormal.php
+++ gentelella-master/production/turntonormal.php
@@ -5,28 +5,31 @@ mysqli_query($conn,'SET NAMES utf8');
 
 $phonenum = $_POST['PhoneNum'];
 
-$sql= "select * from address where phonenum = '$phonenum'";
-$sql1= "select * from personinfo where phonenum = '$phonenum'";
-$delete_sql="delete from address where phonenum='$phonenum'";
-$address_sql= "select * from address";
-
 $response = array();
 $response["inaddress"]=false;
 
+$sql= "select * from address where phonenum = '$phonenum'";
+$res= $conn->query($sql); // 운전자의 위치 정보
 
-$res= $conn->query($sql); #(address) 전송된 phonenum에 해당하는 위도 경도 값
-$res1= $conn->query($sql1); #(personinfo) 전송된 phonenum의 등록 정보
+$sql1= "select * from personinfo where phonenum = '$phonenum'";
+$res1= $conn->query($sql1); // 운전자 등록 여부 확인 
 
-if(mysqli_fetch_array($res1)!=NULL){ //등록 정보 확인
-    $del_res=$conn->query($delete_sql); #(address)해당 긴급자동차의 정보를 삭제
+if(mysqli_fetch_array($res1)!=NULL){ // 등록시 
+    $delete_sql="delete from address where phonenum='$phonenum'";
+    $del_res=$conn->query($delete_sql); // 운전자의 위치정보 모두 삭제
+
     $response["inaddress"]=true;
 }
 $num=0;
-$address_res= $conn->query($address_sql); #(address)모든 address 위치 정보 선택
-// 위치정보 앱에 뿌려주기
-while($row=mysqli_fetch_array($address_res)){
+
+$address_sql= "select * from address";
+$address_res= $conn->query($address_sql); #모든 긴급자동차 운전자의 위치 정보 
+
+while($row=mysqli_fetch_array($address_res)){ 
     $response["num_inaddress"]=$num;
     $numstr = (string)$num;
+    
+    // 긴급자동차 운전자 위치정보 배열 생성
     $response["Latitude$numstr"]= $row["Latitude"];
     $response["Longitude$numstr"]= $row["Longitude"];
     $num++;
